6ec43210664be724203e1409cdb1e22a6469de10
Moosabilal <moosabilal75608@gmail.com>
2026-01-25 14:54:57 +0530

    chore: Remove build artifacts from git

--- backend/dist/Presentation/Routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const AuthController_1 = require("../Controllers/AuthController");
-const container_1 = require("../../Infrastructure/IOC/container");
-const router = (0, express_1.Router)();
-const controller = container_1.container.get(AuthController_1.AuthController);
-router.post("/login", (req, res) => controller.login(req, res));
-exports.default = router;
